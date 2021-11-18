1b1d24c5d085769d5192e6fcbcffa39ca350fbba
Sophia Ju <sophia.ju10@gmail.com>
2021-11-18 17:20:56 -0500

    finished evaluate accuracy script

--- src/README.md
+++ src/README.md
@@ -3,15 +3,15 @@
 ## To get results for Q1.a):
 1. run `get_boundaries.sh input.file output.file strand_code`\
 exact command I used is:\
-`./get_boundaries.sh Vibrio_cholerae.GFC_11.37.gff3 cholerae_bound.txt +`
+`./get_boundaries.sh ../data/Vibrio_cholerae.GFC_11.37.gff3 cholerae/cholerae_bound.txt +`
 
 2. to get average lengths of intergenic and genic regions, run `get_avg_len.py -g gff3.file -b bounds.file -o output.file`\
 exact command I used is:\
-`python get_avg_len.py -g Vibrio_cholerae.GFC_11.37.gff3 -b cholerae_bound.txt -o average_lengths.json`
+`python get_avg_len.py -g ../data/Vibrio_cholerae.GFC_11.37.gff3 -b cholerae/cholerae_bound.txt -o cholerae/average_lengths.json`
 
 3. to get nucleotide frequencies for intergenic regions and codon frequencies for genic regions, run `get_freqs.py -f fasta.file -b bounds.file -o output.file`\
 exact command I used is:\
-`python get_freqs.py -f Vibrio_cholerae.GFC_11.dna.toplevel.fa -b cholerae_bound.txt -o codon_frequencies.json`
+`python get_freqs.py -f ../data/Vibrio_cholerae.GFC_11.dna.toplevel.fa -b cholerae/cholerae_bound.txt -o cholerae/codon_frequencies.json`
 
 ## To get results for Q1.b & c):
 run `python viterbi.py -f fasta.file -c config.json -o output.file`\
@@ -19,8 +19,8 @@ exact command I used is:\
 `python viterbi.py -f ../data/Vibrio_vulnificus.ASM74310v1.dna.toplevel.fa -c config.json -o vulnificus/predictions.gff3`
 
 ## To get results for Q1.d):
-run `get_boundaries.sh input.file output.file strand_code`\
-`python eval_accuracy.py -b bounds.file -g gff3.file -o output.file`\
+run `get_boundaries.sh input.file output.file strand_code` and\
+`python eval_accuracy.py -b bounds.file -g gff3.file`\
 exact commands I used are:\
-`./get_boundaries.sh ../data/Vibrio_vulnificus.ASM74310v1.37.gff3 vulnificus/vulnificus_bound.txt +`
-`python eval_accuracy.py -b vulnificus/vulnificus_bound.txt -g vulnificus/predictions.gff3 -o vulnificus/accuracy.txt`
+`./get_boundaries.sh ../data/Vibrio_vulnificus.ASM74310v1.37.gff3 vulnificus/vulnificus_bound.txt +` and\
+`python eval_accuracy.py -b vulnificus/vulnificus_bound.txt -g vulnificus/predictions.gff3`
--- src/eval_accuracy.py
+++ src/eval_accuracy.py
@@ -5,36 +5,87 @@ from Bio import SeqIO
 import math
 import pandas as pd
 
+def get_match_counts(first_group, sec_group, dict):
+
+    for first_contig in first_group:
+
+        first_contig_name = first_contig[0]
+        first_contig_df = first_contig[1]
+
+        # if the first contig is also in the second file
+        if first_contig_name in sec_group.groups:
+            sec_contig = sec_group.get_group(first_contig_name)
+
+            # get all start and end positions as lists
+            sec_start_list = sec_contig['start'].to_list()
+            sec_end_list = sec_contig['end'].to_list()
+
+            # for each gene in first group
+            for index, row in first_contig_df.iterrows():
+                a_start = row['start']
+                a_end =  row['end']
+
+                # matches neither
+                if a_start not in sec_start_list and a_end not in sec_end_list:
+                    dict['none'] += 1
+                # matches start but not end
+                elif a_start in sec_start_list and a_end not in sec_end_list:
+                    dict['start'] += 1
+                # matches end but not start
+                elif a_start not in sec_start_list and a_end in sec_end_list:
+                    dict['end'] += 1
+                # matches both
+                else:
+                    dict['both'] += 1
+
+        # if first contig not in second, no matches
+        else:
+            # print('no group', first_contig_name)
+            # add all first genes to no matches count
+            dict['none'] += len(first_contig)
+
+def count_to_freq(dict):
+    # make counts into frequencies
+    num_values = sum(dict.values())
+    for key in dict:
+        dict[key] = dict[key] / num_values
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--bounds', help='add the annotated bounds file for prediction')
     parser.add_argument('-g','--gff3', help='add the gff3 file with predictions')
-    parser.add_argument('-o', '--output', help='add output file')
     args = parser.parse_args()
     bounds_file = args.bounds
     gff3_file = args.gff3
-    output_file = args.output
 
-    bounds_df = pd.read_csv(bounds_file, names=['contig', 'start', 'end'], sep=' ')
-    gff3_df = pd.read_csv(gff3_file, names=['contig',2, 'start', 'end', 5, 6, 7, 8], sep='\t')
-    gff3_df.drop(columns=[2,5,6,7,8], inplace=True)
+    annotated_df = pd.read_csv(bounds_file, names=['contig', 'start', 'end'], sep=' ')
+    predicted_df = pd.read_csv(gff3_file, names=['contig',2, 'start', 'end', 5, 6, 7, 8], sep='\t')
+    predicted_df.drop(columns=[2,5,6,7,8], inplace=True)
 
-    print(bounds_df)
-    print(gff3_df)
+    annot_gr = annotated_df.groupby('contig')
+    pred_gr = predicted_df.groupby('contig')
 
-    # annotated (given) genes that
+    # for each contig in the annotated (given) file
+    a_matches = {'both':0, 'start':0, 'end':0, 'none':0}
 
-    # perfectly match both ends of one of predicted
+    get_match_counts(annot_gr, pred_gr, a_matches)
+    count_to_freq(a_matches)
+    print('fraction of annotated genes that: \nperfectly match both ends of one of predicted genes: ', a_matches['both'])
+    print('match the start but not the end of a predicted gene:', a_matches['start'])
+    print('match the end but not the start of a predicted gene:', a_matches['end'])
+    print('do not match either the start or end of a predicted gene:', a_matches['none'])
 
-    # match the start but not the end
-     
-    # match the end but not the start
-
-    # Do not match either the start or end
+    print('\n')
 
     # same with predicted genes
+    p_matches = {'both':0, 'start':0, 'end':0, 'none':0}
+    get_match_counts(pred_gr, annot_gr, p_matches)
+    count_to_freq(p_matches)
+    print('fraction of predicted genes that: \nperfectly match both ends of one of annotated genes: ', p_matches['both'])
+    print('match the start but not the end of a annotated gene:', p_matches['start'])
+    print('match the end but not the start of a annotated gene:', p_matches['end'])
+    print('do not match either the start or end of a annotated gene:', p_matches['none'])
 
 
 
